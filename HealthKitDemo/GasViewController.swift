ec70c2a66e77b9f082523a81fbe6438fcd4c3f42
qiuh1016 <qiuhong@qiuhs-iMac.local>
2016-10-11 16:50:49 +0800

    coreData

--- HealthKitDemo/GasViewController.swift
+++ HealthKitDemo/GasViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
-import HealthKit
+import CoreData
 
 class GasViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
-    
     @IBOutlet weak var wheelImage: UIImageView!
     
+    var wheelRotation: CGFloat = 0.0
+    var rotationLimited: CGFloat = 2 * CGFloat(M_PI) * 240 / 360
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +30,7 @@ class GasViewController: UIViewController {
     }
     
     @IBAction func addButtonTapped(_ sender: AnyObject) {
-       
+       storePerson(price: Double(wheelRotation / rotationLimited * 100), date: Date(), category: 0)
     }
     
     func imageOperation() {
@@ -42,29 +43,46 @@ class GasViewController: UIViewController {
         
     }
     
-    var wheelRotation: CGFloat = 0.0
-    var rotationLimited: CGFloat = CGFloat(M_PI) * 240 / 360
+
     
     @IBAction func panned(_ sender: AnyObject) {
         
         let panGesture = sender as! UIPanGestureRecognizer
         let velocity = panGesture.velocity(in: wheelImage)
         let rotation = CGFloat(M_PI / 360 / 20) * velocity.x
-        print(wheelRotation)
-        if wheelRotation + rotation < -rotationLimited {
-            
-        } else if wheelRotation + rotation > rotationLimited {
-            
-        } else {
+        if wheelRotation + rotation > 0 && wheelRotation + rotation < rotationLimited  {
             self.wheelImage.transform = self.wheelImage!.transform.rotated(by: rotation)
             self.wheelRotation += rotation
         }
         
-        let cg = (wheelRotation + rotationLimited) / (2 * rotationLimited) * 100
+        let cg = wheelRotation / rotationLimited * 100
         let int = lroundf(Float(cg))
         label.text = "\(int)%"
         
+    }
+    
+    func getContext () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    func storePerson(price: Double, date: Date, category: Int){
+        let context = getContext()
+        // 定义一个entity，这个entity一定要在xcdatamodeld中做好定义
+        let entity = NSEntityDescription.entity(forEntityName: "Record", in: context)
         
+        let record = NSManagedObject(entity: entity!, insertInto: context)
+        
+        record.setValue(price, forKey: "price")
+        record.setValue(date, forKey: "date")
+        record.setValue(category, forKey: "category")
+        
+        do {
+            try context.save()
+            print("saved")
+        }catch{
+            print(error)
+        }
     }
     
     
